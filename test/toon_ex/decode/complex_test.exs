6e79039b4851a0e7cb1d58c2a822e2b9c7c5850f
Manh Van Vu <vuvanmanh123@gmail.com>
2026-04-02 08:10:35 +0700

    add new test cases

--- test/toon_ex/decode/complex_test.exs
+++ test/toon_ex/decode/complex_test.exs
@@ -139,6 +139,46 @@ defmodule ToonEx.Decode.ComplexObjectTest do
       assert expected == result
     end
 
+    test "decodes mixed object 3" do
+      toon = """
+      [2]:
+        - "test"
+        - async: false
+          success: true
+          result[3]{name,user_id,username}:
+            Layla Gibson,019d3c0e-0997-7e80-9bd3-024865090b15,user_89
+            Annabelle Jaskolski,019d3c0e-096e-70ac-bcab-39f6ccc7f77c,user_68
+            Mrs. Saige Cassin V,019d3c0e-092e-794b-b233-10c9557bf2a9,user_44
+      """
+
+      expected_data = [
+        "test",
+        %{
+          "async" => false,
+          "success" => true,
+          "result" => [
+            %{
+              "name" => "Layla Gibson",
+              "user_id" => "019d3c0e-0997-7e80-9bd3-024865090b15",
+              "username" => "user_89"
+            },
+            %{
+              "name" => "Annabelle Jaskolski",
+              "user_id" => "019d3c0e-096e-70ac-bcab-39f6ccc7f77c",
+              "username" => "user_68"
+            },
+            %{
+              "name" => "Mrs. Saige Cassin V",
+              "user_id" => "019d3c0e-092e-794b-b233-10c9557bf2a9",
+              "username" => "user_44"
+            }
+          ]
+        }
+      ]
+
+      assert expected_data == ToonEx.decode!(toon)
+    end
+
     test "common object 1" do
       # ↓ indent 6 (was 4) — one extra level inside args:
       toon =
